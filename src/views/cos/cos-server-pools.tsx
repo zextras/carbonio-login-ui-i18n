8a19efa4150dcd5e633f1d61e25ed4a1aa502e3e
zextras_kapil <kapil.nadiyapara@zextras.com>
2022-08-08 16:07:51 +0530

    chore: enable/disable serverpool facility added

--- src/views/cos/cos-server-pools.tsx
+++ src/views/cos/cos-server-pools.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable react-hooks/rules-of-hooks */
 /*
  * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { FC, useEffect, useMemo, useState } from 'react';
+import React, { FC, useCallback, useContext, useEffect, useMemo, useState } from 'react';
 import {
 	Container,
 	Row,
@@ -16,22 +14,30 @@ import {
 	Padding,
 	Input,
 	Button,
-	Icon
+	Icon,
+	Modal,
+	SnackbarManagerContext
 } from '@zextras/carbonio-design-system';
 import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
 import ListRow from '../list/list-row';
 import Paginig from '../components/paging';
 import { useCosStore } from '../../store/cos/store';
 import { getAllServers } from '../../services/get-all-servers-service';
+import { modifyCos } from '../../services/modify-cos-service';
 
 const CosServerPools: FC = () => {
 	const [t] = useTranslation();
+	const { cosId }: { cosId: string } = useParams();
 	const cosInformation = useCosStore((state) => state.cos?.a);
-	const [zimbraMailHostPool, setZimbraMailHostPool] = useState<boolean>(false);
+	const [zimbraMailHostPool, setZimbraMailHostPool] = useState<boolean>(true);
 	const [serverList, setServerList] = useState<Array<any>>([]);
 	const [zimbraMailHostPoolList, setZimbraMailHostPoolList] = useState<Array<any>>([]);
 	const [serverTableRows, setServerTableRows] = useState<Array<any>>([]);
 	const [selectedTableRows, setSelectedTableRows] = useState<Array<any>>([]);
+	const [openConfirmDialog, setOpenConfirmDialog] = useState<boolean>(false);
+	const createSnackbar: any = useContext(SnackbarManagerContext);
+	const setCos = useCosStore((state) => state.setCos);
 
 	const getAllServer = (): any => {
 		getAllServers()
@@ -85,12 +91,12 @@ const CosServerPools: FC = () => {
 	useEffect(() => {
 		if (!!cosInformation && cosInformation.length > 0) {
 			getAllServer();
-			const _zimbraMailHostPool: any = cosInformation.find(
-				(item: any) => item?.n === 'zimbraMailHostPool'
-			);
-			if (_zimbraMailHostPool) {
-				setZimbraMailHostPool(_zimbraMailHostPool?.c);
-			}
+			// const _zimbraMailHostPool: any = cosInformation.find(
+			// 	(item: any) => item?.n === 'zimbraMailHostPool'
+			// );
+			// if (_zimbraMailHostPool) {
+			// 	setZimbraMailHostPool(_zimbraMailHostPool?.c);
+			// }
 
 			const list = cosInformation.filter((item: any) => item?.n === 'zimbraMailHostPool');
 			if (list) {
@@ -121,6 +127,98 @@ const CosServerPools: FC = () => {
 		],
 		[t]
 	);
+
+	const onDisable = useCallback(() => {
+		setOpenConfirmDialog(true);
+	}, []);
+
+	const onModifyCOS = useCallback(
+		(body) => {
+			modifyCos(body)
+				.then((response) => response.json())
+				.then((data) => {
+					const cos: any = data?.Body?.ModifyCosResponse?.cos[0];
+					if (cos) {
+						createSnackbar({
+							key: 'success',
+							type: 'success',
+							label: t(
+								'label.the_last_changes_has_been_saved_successfully',
+								'The last changes has been saved successfully'
+							),
+							autoHideTimeout: 3000,
+							hideButton: true,
+							replace: true
+						});
+						setCos(cos);
+					} else {
+						createSnackbar({
+							key: 'error',
+							type: 'error',
+							label: data?.Body?.Fault?.Reason?.Text,
+							autoHideTimeout: 3000,
+							hideButton: true,
+							replace: true
+						});
+					}
+					setOpenConfirmDialog(false);
+					setSelectedTableRows([]);
+				});
+		},
+		[createSnackbar, t, setCos]
+	);
+
+	const onEnable = useCallback(() => {
+		const attributes: any[] = [];
+		const body: any = {};
+		body._jsns = 'urn:zimbraAdmin';
+		zimbraMailHostPoolList.forEach((item: any) => {
+			attributes.push({
+				n: 'zimbraMailHostPool',
+				_content: item?._content
+			});
+		});
+		attributes.push({
+			n: 'zimbraMailHostPool',
+			_content: selectedTableRows[0]
+		});
+		body.a = attributes;
+		body.id = {
+			_content: cosId
+		};
+		onModifyCOS(body);
+	}, [selectedTableRows, zimbraMailHostPoolList, onModifyCOS, cosId]);
+
+	const onDisableServer = useCallback(() => {
+		const allServers = zimbraMailHostPoolList.filter(
+			(item: any) => item?._content !== selectedTableRows[0]
+		);
+		const attributes: any[] = [];
+		const body: any = {};
+		body._jsns = 'urn:zimbraAdmin';
+		if (allServers.length === 0) {
+			attributes.push({
+				n: 'zimbraMailHostPool',
+				_content: ''
+			});
+		} else if (allServers.length > 0) {
+			allServers.forEach((item: any) => {
+				attributes.push({
+					n: 'zimbraMailHostPool',
+					_content: item?._content
+				});
+			});
+		}
+		body.a = attributes;
+		body.id = {
+			_content: cosId
+		};
+		onModifyCOS(body);
+	}, [selectedTableRows, zimbraMailHostPoolList, onModifyCOS, cosId]);
+
+	const hideConfirmDialog = useCallback(() => {
+		setOpenConfirmDialog(false);
+	}, []);
 
 	return (
 		<Container mainAlignment="flex-start" crossAlignment="flex-start">
@@ -156,89 +254,141 @@ const CosServerPools: FC = () => {
 									'cos.limt_serverpool_avaiable_create_user',
 									'Limit server pool available for creating new users in this COS'
 								)}
+								onClick={(): void => {
+									setZimbraMailHostPool(!zimbraMailHostPool);
+								}}
 							/>
 						</Padding>
 					</ListRow>
-					<Row
-						takeAvwidth="fill"
-						mainAlignment="flex-start"
-						width="100%"
-						padding={{ top: 'small', bottom: 'small' }}
-					>
-						<Container
-							orientation="vertical"
-							mainAlignment="space-around"
-							background="gray6"
-							height="58px"
-						>
+					{zimbraMailHostPool && (
+						<>
 							<Row
-								orientation="horizontal"
+								takeAvwidth="fill"
 								mainAlignment="flex-start"
-								crossAlignment="flex-start"
 								width="100%"
+								padding={{ top: 'small', bottom: 'small' }}
 							>
-								<Row mainAlignment="flex-start" width="70%" crossAlignment="flex-start">
-									<Input
-										value={''}
-										label={t('cos.search_a_specific_server', 'Search a specific server')}
-										CustomIcon={(): any => (
-											<Icon icon="FunnelOutline" size="large" color="secondary" />
-										)}
-									/>
-								</Row>
+								<Container
+									orientation="vertical"
+									mainAlignment="space-around"
+									background="gray6"
+									height="58px"
+								>
+									<Row
+										orientation="horizontal"
+										mainAlignment="flex-start"
+										crossAlignment="flex-start"
+										width="100%"
+									>
+										<Row mainAlignment="flex-start" width="70%" crossAlignment="flex-start">
+											<Input
+												value={''}
+												label={t('cos.search_a_specific_server', 'Search a specific server')}
+												CustomIcon={(): any => (
+													<Icon icon="FunnelOutline" size="large" color="secondary" />
+												)}
+											/>
+										</Row>
 
-								<Row width="30%" mainAlignment="flex-start" crossAlignment="flex-start">
-									<Padding left="large" right="large">
-										<Button
-											type="outlined"
-											key="add-button"
-											label={t('label.enable', 'enable')}
-											color="primary"
-											icon="CheckmarkCircleOutline"
-											height={44}
-											iconPlacement="right"
-											disabled={!enable}
-										/>
-									</Padding>
+										<Row width="30%" mainAlignment="flex-start" crossAlignment="flex-start">
+											<Padding left="large" right="large">
+												<Button
+													type="outlined"
+													key="add-button"
+													label={t('label.enable', 'enable')}
+													color="primary"
+													icon="CheckmarkCircleOutline"
+													height={44}
+													iconPlacement="right"
+													disabled={!enable}
+													onClick={onEnable}
+												/>
+											</Padding>
 
-									<Button
-										type="outlined"
-										key="add-button"
-										label={t('label.disable', 'disable')}
-										color="error"
-										icon="CloseCircleOutline"
-										iconPlacement="right"
-										height={44}
-										disabled={!disable}
-									/>
-								</Row>
+											<Button
+												type="outlined"
+												key="add-button"
+												label={t('label.disable', 'disable')}
+												color="error"
+												icon="CloseCircleOutline"
+												iconPlacement="right"
+												height={44}
+												disabled={!disable}
+												onClick={onDisable}
+											/>
+										</Row>
+									</Row>
+								</Container>
 							</Row>
-						</Container>
-					</Row>
-					<ListRow>
-						<Table
-							multiSelect={false}
-							rows={serverTableRows}
-							headers={tableHeader}
-							showCheckbox={false}
-							selectedRows={selectedTableRows}
-							onSelectionChange={(selected: any): void => setSelectedTableRows(selected)}
-						/>
-					</ListRow>
-					<ListRow>
-						<Divider />
-					</ListRow>
-					<ListRow>
-						<Paginig
-							totalItem={1}
-							pageSize={10}
-							setOffset={(): void => {
-								console.log('setOffset for paging');
-							}}
-						/>
-					</ListRow>
+							<ListRow>
+								<Table
+									multiSelect={false}
+									rows={serverTableRows}
+									headers={tableHeader}
+									showCheckbox={false}
+									selectedRows={selectedTableRows}
+									onSelectionChange={(selected: any): void => setSelectedTableRows(selected)}
+								/>
+							</ListRow>
+							<ListRow>
+								<Divider />
+							</ListRow>
+							<ListRow>
+								<Paginig
+									totalItem={1}
+									pageSize={10}
+									setOffset={(): void => {
+										console.log('setOffset for paging');
+									}}
+								/>
+							</ListRow>
+						</>
+					)}
 				</Container>
 			</Container>
+
+			<Modal
+				title={`${t('cos.disabling_pool', 'Disabling pool')}`}
+				open={openConfirmDialog}
+				showCloseIcon
+				onClose={(): void => {
+					setOpenConfirmDialog(false);
+				}}
+				customFooter={
+					<Container orientation="horizontal" mainAlignment="space-between">
+						<Button
+							label={t('label.helo', 'Help')}
+							type="outlined"
+							color="primary"
+							onClick={hideConfirmDialog}
+						/>
+						<Container orientation="horizontal" mainAlignment="flex-end">
+							<Padding all="small">
+								<Button
+									label={t('label.no_go_back', 'No, Go Back')}
+									color="secondary"
+									onClick={hideConfirmDialog}
+								/>
+							</Padding>
+							<Button
+								label={t('cos.yes_disable', 'Yes, Disable')}
+								color="error"
+								onClick={onDisableServer}
+							/>
+						</Container>
+					</Container>
+				}
+			>
+				<Padding all="medium">
+					<Text overflow="break-word" weight="regular">
+						{t('cos.create_cos_success_msg', {
+							serverName: serverList.find((sp: any) => sp?.id === selectedTableRows[0])?.name,
+							defaultValue:
+								'You are disabling pool on {{serverName}}. All mailboxes will be not moved. Are you sure you want to delete it?'
+						})}
+					</Text>
+				</Padding>
+			</Modal>
 		</Container>
 	);
 };
