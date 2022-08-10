b1b2ddf5b2794660d6f0e727de0a2f2e81e58ef7
zextras_kapil <kapil.nadiyapara@zextras.com>
2022-08-10 18:50:49 +0530

    fix: reset field after update the account

--- src/views/domain/manange/restore-delete-account/restore-delete-account.tsx
+++ src/views/domain/manange/restore-delete-account/restore-delete-account.tsx
@@ -9,11 +9,30 @@ import { Container, SnackbarManagerContext } from '@zextras/carbonio-design-syst
 import { useTranslation } from 'react-i18next';
 import RestoreAccountWizard from './restore-delete-account-wizard';
 import { doRestoreDeleteAccount } from '../../../../services/restore-delete-account-service';
+import { RestoreDeleteAccountContext } from './restore-delete-account-context';
 
 const RestoreDeleteAccount: FC = () => {
 	const [t] = useTranslation();
 	const createSnackbar: any = useContext(SnackbarManagerContext);
 	const [showRestoreAccountWizard, setShowRestoreAccountWizard] = useState<boolean>(false);
+	const context = useContext(RestoreDeleteAccountContext);
+	const { restoreAccountDetail, setRestoreAccountDetail } = context;
+	const resetAllFields = useCallback(() => {
+		setRestoreAccountDetail((prev: any) => ({
+			...prev,
+			name: '',
+			id: '',
+			status: '',
+			createDate: '',
+			copyAccount: '',
+			dateTime: null,
+			lastAvailableStatus: false,
+			hsmApply: false,
+			dataSource: false,
+			isEmailNotificationEnable: false,
+			notificationReceiver: ''
+		}));
+	}, [setRestoreAccountDetail]);
 	const restoreAccountRequest = useCallback(
 		(
 			name,
@@ -42,6 +61,7 @@ const RestoreDeleteAccount: FC = () => {
 				.then((response) => response.json())
 				.then((data) => {
 					const error = data?.error?.details?.cause || data?.error?.message;
+					const success = data?.operationId;
 					if (error) {
 						createSnackbar({
 							key: 'error',
@@ -52,9 +72,23 @@ const RestoreDeleteAccount: FC = () => {
 							replace: true
 						});
 					}
+					if (success) {
+						createSnackbar({
+							key: 'success',
+							type: 'success',
+							label: t(
+								'label.restore_account_has_added_operation_queue',
+								'The restore account has been added on operation queue successfull'
+							),
+							autoHideTimeout: 3000,
+							hideButton: true,
+							replace: true
+						});
+						resetAllFields();
+					}
 				});
 		},
-		[createSnackbar]
+		[createSnackbar, t, resetAllFields]
 	);
 
 	return (
